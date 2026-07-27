68392e3f2ccd8c7e130f07c55eabbd0e5f557054
RAHUL-VERMA-CODE <145197758+RAHUL-VERMA-CODE@users.noreply.github.com>
2026-07-28 00:25:47 +0530

    setup.py file

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns the list of requirements.
+    """
+    requirements = []
+
+    try:
+        with open(file_path, "r") as file:
+            requirements = file.readlines()
+            requirements = [req.strip() for req in requirements]
+
+            if HYPHEN_E_DOT in requirements:
+                requirements.remove(HYPHEN_E_DOT)
+
+    except FileNotFoundError:
+        print(f"{file_path} not found.")
+
+    return requirements
+
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Rahul Verma",
+    author_email="your_email@example.com", 
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
